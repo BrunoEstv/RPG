f3ecfd5cc328e857f2a3b51d5e5271f288f58252
BrunoEstv <113032334+BrunoEstv@users.noreply.github.com>
2024-09-10 07:18:42 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Magias",
+        descricao: "Em Dungeons & Dragons, a magia é uma força poderosa e versátil, moldada pela imaginação dos jogadores e mestres. Seja lançada por magias, feitiços ou poderes divinos, ela pode curar ferimentos, manipular o ambiente, invocar criaturas e até mesmo alterar a realidade.",
+        link: "https://avribacki.gitlab.io/magias5e/",
+        tags: "magia feitiço poder curar"
+    },
+    {
+        titulo: "Raças",
+        descricao: "As raças em Dungeons & Dragons representam diferentes origens e características, como humanos, elfos, anões, halflings, e muitas outras. Cada raça possui habilidades únicas, proficiências e traços que influenciam o estilo de jogo do personagem.",
+        link: "https://www.helprpg.com.br/2024/02/d-5e-racas-oficias.html",
+        tags: "raça elfo anões halfling humano personagem"
+    },
+    {
+        titulo: "Classes",
+        descricao: "As classes em Dungeons & Dragons definem a profissão e as habilidades do personagem, como guerreiro, mago, clérigo, ladino e muitas outras. Cada classe possui habilidades únicas, magias e proficiências que moldam o papel do personagem no grupo.",
+        link: "https://nuckturp.com.br/o-guia-de-classes-de-dd-5e/",
+        tags: "classe guerreiro mago clérigo ladino proficiência"
+    }
+];
